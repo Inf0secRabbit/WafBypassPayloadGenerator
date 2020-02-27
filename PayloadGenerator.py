d03e417b12432b622dcf995bc3b14813d6bbe32b
Spoiler0X01 <maroofmansuri42@gmail.com>
2020-02-27 18:25:53 +0530

    Add files via upload

---
+++ PayloadGenerator.py
@@ -0,0 +1,16 @@
+payload = input("Enter your payload")
+retVal = payload
+if payload:
+    retVal = ""
+    i = 0
+
+    while i < len(payload):
+        if payload[i] == '%' and (i < len(payload) - 2) and payload[i + 1:i + 2] in string.hexdigits and payload[i + 2:i + 3] in string.hexdigits:
+            retVal += '%%25%s' % payload[i + 1:i + 3]
+            i += 3
+        else:
+            retVal += '%%25%.2X' % ord(payload[i])
+            i += 1
+
+print(retVal)
+
